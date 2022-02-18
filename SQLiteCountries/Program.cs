1cd056b4d4a90195d2eb5a82dac3c9aa4dbe8371
Opilane TTHK <opilane@tthk.ee>
2022-02-18 11:26:31 +0200

    SQLITE

---
+++ SQLiteCountries/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Data.SQLite;
+
+namespace SQLiteCountries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadCountries();
+        }
+        private static void ReadCountries()
+        {
+            Database databaseObj = new Database();
+            string query = "SELECT Country.countryname, Capital.capitalname from Capital join country on Capital.countryid = Country.rowid";
+            SQLiteCommand myCommend = new SQLiteCommand(query, databaseObj.myConnection);
+            databaseObj.OpenConnection();
+            SQLiteDataReader data = myCommend.ExecuteReader();
+
+            if(data.HasRows)
+            {
+                while(data.Read())
+                {
+                    Console.WriteLine($"Country: {data["Country"]}. Capital: {data["Capital"]}");
+                }
+            }
+            databaseObj.CloseConnection();
+        }
+    
+    
+    
+    
+    }
+}
